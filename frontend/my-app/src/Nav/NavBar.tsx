b38a9c0ab42f58bc9d3cd43006b97df5c43a19b3
Biswajit <paridabiswa2k@gmail.com>
2025-04-15 00:11:34 +0530

    setting up thr frontend

---
+++ frontend/my-app/src/Nav/NavBar.tsx
@@ -0,0 +1,26 @@
+
+
+function NavBar(){
+
+    return (
+        <>
+        <div className="nav">
+            <div className="appname">
+               <h1>Intro</h1> 
+               
+            </div>
+            <div className="links">
+                <a href="">register</a>
+                <a href="">login</a>
+                <a href="">dashboard</a>
+                <a href="">topics</a>
+
+            </div>
+
+        </div>
+
+        
+        </>
+    )
+}
+export default NavBar
