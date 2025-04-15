1ddf5ce690d02f47c2e804cfb125daefee0c2443
Biswajit <paridabiswa2k@gmail.com>
2025-04-15 23:55:04 +0530

    setting up thr frontend

---
+++ frontend/my-app/src/auth/Register.tsx
@@ -0,0 +1,13 @@
+function Register(){
+
+    return(
+        <>
+        <label htmlFor="">name</label>
+        <input type="text" />
+        <label htmlFor="">emailid</label>
+        <label htmlFor="">password</label>
+        <button>login via google</button>
+        </>
+    )
+}
+export default Register;
