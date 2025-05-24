4ef8d6ed6d184c148e196a4e66695efa34addd16
Biswajit <paridabiswa2k@gmail.com>
2025-05-24 22:36:11 +0530

    add some servieces

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,7 +9,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors())
-
+app.get('/',(req,res)=>{
+  res.send('hey absolute fuck')
+})
 
 const port=8080
 
